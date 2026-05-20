a56264436d722ee2b48e7e52c89a0b1813c99b90
Pramod Kaushal Fernando <pramodkaushalgd@gmail.com>
2026-05-20 13:32:34 +0530

    Admin Dashbord Design

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,11 +4,30 @@ namespace UniManage.Controllers
 {
     public class AccountController : Controller
     {
+        // LOGIN PAGE
         public IActionResult Login()
         {
             return View();
         }
 
+        // LOGIN PROCESS
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            // TEMP LOGIN
+
+            if (email == "admin@gmail.com"
+                && password == "123")
+            {
+                return RedirectToAction("Dashboard", "Admin");
+            }
+
+            ViewBag.Error = "Invalid Email or Password";
+
+            return View();
+        }
+
+        // REGISTER PAGE
         public IActionResult Register()
         {
             return View();
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniManage.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult Courses()
+        {
+            return View();
+        }
+
+        public IActionResult Enrollments()
+        {
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
