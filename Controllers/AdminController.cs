dbd920b3a5cb6c84f529c84ebb1490e6d5b77659
Pramod Kaushal Fernando <pramodkaushalgd@gmail.com>
2026-05-20 13:32:34 +0530

    Admin Dashbord Design

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniManage.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult Modules()
+        {
+            return View();
+        }
+
+        public IActionResult Enrollments()
+        {
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+
+        public IActionResult AddLecturer()
+        {
+            return View();
+        }
+
+        public IActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        public IActionResult AddModule()
+        {
+            return View();
+        }
+
+        public IActionResult Courses()
+        {
+            return View();
+        }
+
+        public IActionResult AddCourse()
+        {
+            return View();
+        }
+    }
+}
